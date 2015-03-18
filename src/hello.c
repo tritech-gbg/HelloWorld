6ebdfbad6d25bc9ccb028814463fae5c4a3500bb
tritech-gbg <rickard.bremer@tritech.se>
2015-03-18 09:10:29 +0100

    Hello jenkins!

--- src/hello.c
+++ src/hello.c
@@ -57,7 +57,7 @@ main (int argc, char *argv[])
 #endif
 
   /* Having initialized gettext, get the default message. */
-  greeting_msg = _("Hello, world!");
+  greeting_msg = _("Hello, jenkins!");
 
   /* Even exiting has subtleties.  On exit, if any writes failed, change
      the exit status.  The /dev/full device on GNU/Linux can be used for
